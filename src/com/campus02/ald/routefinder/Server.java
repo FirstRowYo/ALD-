b4839fe4de41ee6f2bf8f4fd0ae0c7e73a66b2e9
Philipp <Philipp@172.31.35.29>
2016-06-11 07:34:26 +0200

    do

---
+++ src/com/campus02/ald/routefinder/Server.java
@@ -0,0 +1,25 @@
+package com.campus02.ald.routefinder;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) 
+	{
+		try (ServerSocket ssock = new ServerSocket(8888);)
+		{
+			Socket client;
+			while (true)
+			{
+				client = ssock.accept();
+				dosomething(); 
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
