96b91f9089cec32052e963201dfdd6b8ac5eeff4
Philipp <Philipp@172.31.35.29>
2016-06-11 09:32:38 +0200

    welcome to the machine

---
+++ src/com/campus02/ald/datastructures/WeightedEdge.java
@@ -0,0 +1,11 @@
+package com.campus02.ald.datastructures;
+
+public class WeightedEdge {
+	public int vertex;
+	public int weight;
+
+	public WeightedEdge(int v, int weight) {
+		this.vertex = v;
+		this.weight = weight;
+	}
+}
---
+++ src/com/campus02/ald/routefinder/Waypoint.java
@@ -0,0 +1,10 @@
+package com.campus02.ald.routefinder;
+
+
+public class Waypoint {
+	
+	public int id;
+	public String name;
+	
+
+}
