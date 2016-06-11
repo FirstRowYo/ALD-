ea8085efb016cff7cd2904e5ec38d470ad18c28a
Stefan Moser <stevemoser@gmx.net>
2016-06-11 07:36:45 +0200

    Testkommentar hinzugefügt

--- src/com/campus02/ald/routefinder/Server.java
+++ src/com/campus02/ald/routefinder/Server.java
@@ -14,7 +14,7 @@ public class Server {
 			while (true)
 			{
 				client = ssock.accept();
-				dosomething(); 
+				dosomething(); //geil
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
