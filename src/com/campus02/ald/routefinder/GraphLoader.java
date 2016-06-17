80487941b58f238f45bd9ea47d0ff06fda6c9d28
Philipp <Philipp@172.31.35.29>
2016-06-17 18:19:44 +0200

    Baum in Graph eingebaut

--- src/com/campus02/ald/routefinder/GraphLoader.java
+++ src/com/campus02/ald/routefinder/GraphLoader.java
@@ -13,12 +13,59 @@ public class GraphLoader {
 	
 	private File file;
 	private ArrayList<String> list = new ArrayList<>();
+	private BaseTree<Integer,String> ctree = new BaseTree<Integer,String>() {
+		@Override
+		protected int compareKey(Integer a, Integer b) {
+			if (a<b)
+				return -1;
+			if (a>b)
+				return 1;
+			else 
+				return 0;}
+		protected int compareValue(String a, String b) {
+			// TODO Auto-generated method stub
+			return a.compareTo(b);}};
+	private BaseTree<String, Integer> stree = new BaseTree<String, Integer>() {
+				@Override
+				protected int compareKey(String a, String b) {
+					return a.compareTo(b);}
+				@Override
+				protected int compareValue(Integer a, Integer b) {
+					if (a<b)
+						return -1;
+					if (a>b)
+						return 1;
+					else 
+						return 0;}};
 	
-	public GraphLoader(String path) {
-		super();
+	
+	public GraphLoader(String path) 
+	{
 		File file = new File(path);
+		buildTree();
 	}
 	
+	private void buildTree() {
+		try (BufferedReader br = new BufferedReader(new FileReader(file));)
+		{
+		String line;
+		Integer counter = 0;
+		while ((line=br.readLine())!=null)
+		{
+			String[]array=line.split(";");
+			String node = array[0];
+			if (ctree.find(node) == null){
+			counter++;}
+			ctree.add(counter,node);
+			stree.add(node, counter);
+		}
+		} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	}
+
 	public void loadGraph() {
 		try (
 			FileReader fr = new FileReader(file);
@@ -28,8 +75,8 @@ public class GraphLoader {
 			String line;
 			while((line=br.readLine()) != null) {
 				String[] array = line.split(";");
-				int idStart = 1; //Methode(array[0]);
-				int idZiel = 2; //Methode(array[1]);
+				int idStart = ctree.find(array[0]).key; //Methode(array[0]); 
+				int idZiel = ctree.find(array[1]).key; //Methode(array[1]);
 				graph.addEdge(idStart, idZiel, Integer.parseInt(array[2]));
 			}
 		} catch (IOException e) {
