4c0c0efee1754d783ca7c08f1cac40a83dfd7806
david <david@172.31.34.74>
2016-06-18 13:29:41 +0200

    Tiefensuche

--- src/com/campus02/ald/routefinder/Tiefensuche.java
+++ src/com/campus02/ald/routefinder/Tiefensuche.java
@@ -3,26 +3,48 @@ package com.campus02.ald.routefinder;
 import java.util.ArrayList;
 
 import com.campus02.ald.datastructures.ListGraph;
+import com.campus02.ald.datastructures.WeightedEdge;
 
 public class Tiefensuche {
 	
 	private int start;
 	private int ziel;
-	private GraphLoader gl=new GraphLoader();
+	private GraphLoader gl;
+	private ListGraph graph;
+	private ArrayList<WeightedEdge>edgelist;
+	private ArrayList<Integer>visetedNode=new ArrayList<>();;
+	
+	
+	
 	//private ListGraph lg=new ListGraph(numVertices, directed)
 	
-	public Tiefensuche(int start, int ziel) {
+	public Tiefensuche(int start, int ziel, GraphLoader gl) {
 		super();
 		this.start = start;
 		this.ziel = ziel;
+		this.gl=gl;
+		this.graph=gl.getGraph();
+		
 	}
 	
-	public ArrayList<Integer>suche()
+	public ArrayList<Integer>search()
 	{
-		ArrayList<Integer>listID=new ArrayList<>();
-		
-		if(gl.loadGraph())
-		
+		edgelist=(ArrayList<WeightedEdge>) graph.getEdges(start);
+		for (WeightedEdge weightedEdge : edgelist) 
+		{
+			search(weightedEdge.vertex);
+		}
+		return null;	
+	}
+	
+	public ArrayList<Integer>search(int current)
+	{
+		if(current==ziel)
+		{
+			return visetedNode;
+		}
+		visetedNode.add(current);
+		search(current+1);
 	}
 	
 	
